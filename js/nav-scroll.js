57b89f7d3ea48b9646d23cffcda87d3e3760c815
atadamowicz <at.adamowicz@gmail.com>
2023-12-30 12:39:16 +0100

    Implemented smoother  website behavior when nav becomes fixed upon scrolling

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,18 +1,24 @@
 export function initializeFixedNav() {
   const nav = document.querySelector(".nav");
+  const navPlaceholder = document.getElementById("nav-placeholder");
 
   function checkNavbarPosition() {
     const scrollPosition = window.scrollY;
     const viewportHeight = window.innerHeight;
+    const navHeight = nav.offsetHeight;
 
     if (scrollPosition >= viewportHeight) {
       nav.classList.add("fixed-nav");
-      document.body.style.paddingTop = nav.offsetHeight + "px";
+      navPlaceholder.style.display = "block";
+      navPlaceholder.style.height = navHeight + "px";
     } else {
       nav.classList.remove("fixed-nav");
-      document.body.style.paddingTop = 0;
+      navPlaceholder.style.display = "none";
+      navPlaceholder.style.height = "0";
     }
   }
 
   window.addEventListener("scroll", checkNavbarPosition);
+  window.addEventListener("load", checkNavbarPosition);
+  window.addEventListener("resize", checkNavbarPosition);
 }
