0d0732d5c9546f60642ed46113a13cf0ce559358
atadamowicz <at.adamowicz@gmail.com>
2023-12-30 11:08:50 +0100

    smooth nav scrolling added

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,8 +1,19 @@
 export const navToggle = () => {
   const toggleButton = document.querySelector(".nav__toggle-button");
   const navLinks = document.querySelector(".nav__list");
+  const navItems = navLinks.querySelectorAll("a"); // Zakładam, że linki to elementy <a>
 
-  toggleButton.addEventListener("click", () => {
+  const toggleNav = () => {
     navLinks.classList.toggle("nav__list--active");
+  };
+
+  toggleButton.addEventListener("click", toggleNav);
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (navLinks.classList.contains("nav__list--active")) {
+        toggleNav();
+      }
+    });
   });
 };
